2104f4f4a46e658242ffc711abe05013f97dca47
Melvin Barney <mdbarney@me.com>
2015-06-12 00:27:46 -0500

    added support for dungeon info output to a csv file

--- recordpeeker/mitmdump_input.py
+++ recordpeeker/mitmdump_input.py
@@ -18,14 +18,27 @@ from recordpeeker.dispatcher import Dispatcher
 current_dungeon_id = 0
 
 # TODO
-# def save_battles(data):
-
-# TODO
-def save_dungeons(data):
+def save_battles(data):
     # temp.append([dungeon_id, series_id, id, name, difficulty, type, rounds, stamina, has_boss])
-    with open('test.csv', 'wb') as f:
-        writer = csv.writer(f)
-        writer.writerows(someiterable)
+    # temp = ["dungeon_id", "series_id", "id", "name", "difficulty", "type", "rounds", "stamina", "has_boss"]
+    file_path = os.getcwd() + "/data/battle_log.csv"
+    if (os.path.isfile(file_path) == False):
+        data.insert(0, ["dungeon_id", "series_id", "id", "name", "difficulty", "type", "rounds", "stamina", "has_boss"])
+        with open(file_path, 'w') as f:
+            writer = csv.writer(f, delimiter='\t',quoting=csv.QUOTE_NONE)
+            writer.writerows(data)
+    else:
+        with open(file_path, 'a') as f:
+            writer = csv.writer(f, delimiter='\t',quoting=csv.QUOTE_NONE)
+            writer.writerows(data)
+
+        
+# # TODO
+# def save_dungeons(data):
+#     # temp.append([dungeon_id, series_id, id, name, difficulty, type, rounds, stamina, has_boss])
+#     with open('test.csv', 'wb') as f:
+#         writer = csv.writer(f)
+#         writer.writerows(data)
 
 
 def get_enemy_stats_from_json(dungeon_id):
@@ -79,12 +92,13 @@ def get_drops(enemy):
 
 
 def handle_get_battle_init_data(data):
-    # enemy_stats_file = open("enemy.json", 'w')
     enemy_list = []
 
-    # # prints out current data list
-    # test_file = open("data.json", 'w')
-    # print >> test_file, json.dumps(data, indent=4, sort_keys=True)
+    # log data
+    debug_path = os.getcwd() + "/debug/handle_get_battle_init_data.json" 
+    test_file = open(debug_path, 'w')
+    print >> test_file, json.dumps(data, indent=4, sort_keys=True)
+    test_file.close()
 
     battle_data = data["battle"]
     battle_id = battle_data["battle_id"]
@@ -131,15 +145,19 @@ def handle_get_battle_init_data(data):
 
             enemy_list.append(enemy)
 
-        # print >> enemy_stats_file, json.dumps(round_data["enemy"], indent=4, sort_keys=True)
-        # print >> enemy_stats_file, json.dumps(enemy_list, indent=4, sort_keys=True)
     save_enemy_stats(enemy_list, battle_id)
 
     print tabulate(tbl, headers="firstrow")
     print ""
-    # enemy_stats_file.close()
 
 def handle_party_list(data):
+
+    # log data
+    debug_path = os.getcwd() + "/debug/handle_party_list.json" 
+    test_file = open(debug_path, 'w')
+    print >> test_file, json.dumps(data, indent=4, sort_keys=True)
+    test_file.close()
+
     wanted = "name series_id acc atk def eva matk mdef mnd series_acc series_atk series_def series_eva series_matk series_mdef series_mnd"
     topn = OrderedDict()
     topn["atk"] = 5
@@ -174,42 +192,139 @@ def handle_party_list(data):
     save_equipment_list(data)
 
 def handle_dungeon_list(data):
-    temp = []
+
+    # log data
+    debug_path = os.getcwd() + "/debug/handle_dungeon_list.json" 
+    test_file = open(debug_path, 'w')
+    print >> test_file, json.dumps(data, indent=4, sort_keys=True)
+    test_file.close()
+
     tbl = []
     world_data = data["world"]
     world_id = world_data["id"]
     world_name = world_data["name"]
     print "Dungeon List for {0} (id={1})".format(world_name, world_id)
     dungeons = data["dungeons"]
-    series_id = dungeons["series_id"]
     for dungeon in dungeons:
         name = dungeon["name"]
-        dungeon_id = dungeon["dungeon_id"]
         id = dungeon["id"]
         difficulty = dungeon["challenge_level"]
         type = "ELITE" if dungeon["type"] == 2 else "NORMAL"
-        rounds = dungeon["round_num"]
-        stamina = dungeon["stamina"]
-        has_boss = dungeon["has_boss"]
         tbl.append([name, id, difficulty, type])
-        temp.append([dungeon_id, series_id, id, name, difficulty, type, rounds, stamina, has_boss])
     tbl = sorted(tbl, key=lambda row : int(row[1]))
     tbl.insert(0, ["Name", "ID", "Difficulty", "Type"])
-    save_dungeons(temp)
     print tabulate(tbl, headers="firstrow")
 
 def handle_battle_list(data):
+
+    # log data
+    debug_path = os.getcwd() + "/debug/handle_battle_list.json" 
+    test_file = open(debug_path, 'w')
+    print >> test_file, json.dumps(data, indent=4, sort_keys=True)
+    test_file.close()
+    
+    temp = []
     tbl = [["Name", "Id", "Rounds"]]
     dungeon_data = data["dungeon_session"]
     dungeon_id = dungeon_data["dungeon_id"]
     dungeon_name = dungeon_data["name"]
     dungeon_type = int(dungeon_data["type"])
     world_id = dungeon_data["world_id"]
+    series_id = dungeon_data["series_id"]
+    difficulty = dungeon_data["challenge_level"]
+    Type = dungeon_data["type"]
     print "Entering dungeon {0} ({1})".format(dungeon_name, "Elite" if dungeon_type==2 else "Normal")
     battles = data["battles"]
+
     for battle in battles:
+        d_id = battle["dungeon_id"]
+        has_boss = battle["has_boss"]
+        ID = battle["id"]
+        name = battle["name"]
+        rounds = battle["round_num"]
+        stamina = battle["stamina"]
         tbl.append([battle["name"], battle["id"], battle["round_num"]])
+        temp.append([d_id, series_id, ID, name, difficulty, Type, rounds, stamina, has_boss])
+    save_battles(temp)
     print tabulate(tbl, headers="firstrow")
+
+# {
+#       "dungeon_id": 620072,
+#       "has_boss": 0,
+#       "id": 720226,
+#       "is_unlocked": false,
+#       "name": "Gil Greenwood - Heroic, Part 1",
+#       "order_no": 1,
+#       "rank": 0,
+#       "round_num": 3,
+#       "stamina": 15
+#     }
+
+# "dungeon_session": {
+#     "background_image_path": "/dff/static/lang/image/dungeon/620072_bg.png",
+#     "battle_id": 720226,
+#     "bgm": "bgm_06_019",
+#     "challenge_level": 80,
+#     "closed_at": 1434268799,
+#     "dungeon_id": 620072,
+#     "kept_out_at": 1434009599,
+#     "max_supporter_ss_gauge": 2,
+#     "name": "Gil Greenwood (VI) - Heroic",
+#     "opened_at": 1433664000,
+#     "order_no": 1,
+#     "party_status": {
+#       "1": {
+#         "ability_1_num": 4,
+#         "ability_2_num": 8,
+#         "hp": 4272,
+#         "no": 1,
+#         "soul_strike_gauge": 0,
+#         "status_ailments": [],
+#         "user_buddy_id": 8614210
+#       },
+#       "2": {
+#         "ability_1_num": 8,
+#         "ability_2_num": 4,
+#         "hp": 3773,
+#         "no": 2,
+#         "soul_strike_gauge": 0,
+#         "status_ailments": [],
+#         "user_buddy_id": 11528112
+#       },
+#       "3": {
+#         "ability_1_num": 4,
+#         "ability_2_num": 4,
+#         "hp": 3864,
+#         "no": 3,
+#         "soul_strike_gauge": 0,
+#         "status_ailments": [],
+#         "user_buddy_id": 9724956
+#       },
+#       "4": {
+#         "ability_1_num": 10,
+#         "ability_2_num": 2,
+#         "hp": 2628,
+#         "no": 4,
+#         "soul_strike_gauge": 0,
+#         "status_ailments": [],
+#         "user_buddy_id": 11532741
+#       },
+#       "5": {
+#         "ability_1_num": 6,
+#         "ability_2_num": 8,
+#         "hp": 4391,
+#         "no": 5,
+#         "soul_strike_gauge": 0,
+#         "status_ailments": [],
+#         "user_buddy_id": 12389534
+#       }
+#     },
+#     "series_id": 106001,
+#     "supporter_ss_gauge": 2,
+#     "type": 1,
+#     "world_id": 800002
+#   }
+
 
 def handle_survival_event(data):
     # XXX: This maybe works for all survival events...
